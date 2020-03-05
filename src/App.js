563232a8148d1f80cd50d86cadd5026c768c7db5
Dang Vu Nhat Quang <ghitacluber@gmail.com>
2020-03-05 22:04:02 +0700

    2nd commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Fuck you how are you
+          Test Travis CI attach
         </p>
         <a
           className='App-link'
