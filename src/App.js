a0f1abd165e83799e989636cd5b36306da15da77
Dang Vu Nhat Quang <ghitacluber@gmail.com>
2020-03-05 23:39:39 +0700

    1st commit quangdvn

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Test Travis CI attach
+          Hello from QuangDVN
         </p>
         <a
           className='App-link'
